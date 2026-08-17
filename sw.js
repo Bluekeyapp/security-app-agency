9e6ce6ffdc6fa40004960fe8f8149793f1d023e3
Bluekeyapp <duzant97@gmail.com>
2026-08-17 16:12:02 -0400

    Add service worker

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = "security-patrol-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles/app.css",
+  "./src/app.js",
+  "./src/patrol.js",
+  "./src/storage.js",
+  "./manifest.webmanifest",
+  "./assets/icon.svg",
+  "./assets/icon-maskable.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (!response || response.status !== 200 || response.type === "opaque") {
+          return response;
+        }
+
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html")))
+  );
+});
